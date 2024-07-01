f7bf35598804b8346355333f90ec704b6a707db3
dimassagngsptr <dimasageng58@gmail.com>
2024-07-01 23:27:22 +0700

    fix postgres

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
-const PostgresDialect = require("@sequelize/postgres");
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: PostgresDialect,
+    dialect: "postgres", // Gunakan 'postgres' sebagai dialect
     port: process.env.DB_PORT,
     ssl: true,
     clientMinMessages: "notice",
@@ -16,7 +16,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: PostgresDialect,
+    dialect: "postgres", // Gunakan 'postgres' sebagai dialect
     port: process.env.DB_PORT,
     ssl: true,
     clientMinMessages: "notice",
@@ -26,7 +26,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: PostgresDialect,
+    dialect: "postgres", // Gunakan 'postgres' sebagai dialect
     port: process.env.DB_PORT,
     ssl: true,
     clientMinMessages: "notice",
