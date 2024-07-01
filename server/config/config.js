5914801d486e2638dd1ed6bf58bd50846fa81f21
dimassagngsptr <dimasageng58@gmail.com>
2024-07-01 23:25:20 +0700

    fix postgres

--- server/config/config.js
+++ server/config/config.js
@@ -1,25 +1,34 @@
 require("dotenv").config();
-
+const PostgresDialect = require("@sequelize/postgres");
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "postgres",
+    dialect: PostgresDialect,
+    port: process.env.DB_PORT,
+    ssl: true,
+    clientMinMessages: "notice",
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "postgres",
+    dialect: PostgresDialect,
+    port: process.env.DB_PORT,
+    ssl: true,
+    clientMinMessages: "notice",
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "postgres",
+    dialect: PostgresDialect,
+    port: process.env.DB_PORT,
+    ssl: true,
+    clientMinMessages: "notice",
   },
 };
--- server/models/index.js
+++ server/models/index.js
@@ -17,9 +17,11 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
+    config.dialect,
     config.database,
     config.username,
     config.password,
+    config.port,
     config
   );
 }
